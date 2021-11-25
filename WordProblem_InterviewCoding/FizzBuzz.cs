6e53a213b2d0e300f16820b19a588b2a392c1e35
Isaac Nana Gyan Ayeh <nanabarimah22@gmail.com>
2021-11-25 12:41:31 +0000

    FizzBuzz Solution

---
+++ WordProblem_InterviewCoding/FizzBuzz.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace WordProblem_InterviewCoding
+{
+    /*When a number is multiple of three, print “Fizz” instead of a number on the console and if multiple of five then print “Buzz” on the console.
+    For numbers which are multiple of three as well five, print “FizzBuzz” on the console.*/
+    public static class FizzBuzz
+    {
+        public static void Method1(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public static void Method2(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                string str = "";
+                if (i % 3 == 0)
+                {
+                    str += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    str += "Buzz";
+                }
+                if (str.Length == 0)
+                {
+                    str = i.ToString();
+                }
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
--- WordProblem_InterviewCoding/Program.cs
+++ WordProblem_InterviewCoding/Program.cs
@@ -7,20 +7,31 @@ namespace WordProblem_InterviewCoding
         static void Main(string[] args)
         {
             //fibonacci Series
-
+            Console.WriteLine("fibonacci number");
             Console.WriteLine(FibonacciNumbers.Fib(9));
 
             Console.WriteLine(".................................");
 
             //nth catalan number
+            Console.WriteLine("catalan number");
             for (int i = 0; i < 10; i++)
                 Console.WriteLine(CatalanNumber.Calalan(i));
 
             Console.WriteLine(".................................");
 
             //n'th bell number
+            Console.WriteLine("Bell Numbers");
             for (int n = 0; n < 5; n++)
                 Console.WriteLine($"Bell Number of {n} is {BellNumbers.BellNumber(n)}");
+
+            Console.WriteLine(".................................");
+
+            //fizzbuzz
+            Console.WriteLine("fizzbuzz method 1");
+            FizzBuzz.Method1(15);
+            Console.WriteLine(".................................");
+            Console.WriteLine("fizzbuzz method 2");
+            FizzBuzz.Method2(15);
         }
     }
 }
